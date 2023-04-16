17b67b25c490292c7701c3944dc10c0f7d2d53ad
amangorach <91610050+amangorach@users.noreply.github.com>
2023-04-16 11:21:24 +0530

    Add files via upload

---
+++ js/Tour.js
@@ -0,0 +1,104 @@
+AFRAME.registerComponent("tour", {
+  init: function () {
+    this.placesContainer = this.el;
+    this.createCards();
+  },
+
+  createCards: function () {
+    const thumbNailsRef = [
+      {
+        id: "taj-mahal",
+        title: "Amazing Spiderman",
+        url: "./assets/1.png",
+      },
+      {
+        id: "budapest",
+        title: "Spiderman",
+        url: "./assets/2.png",
+      },
+
+      {
+        id: "eiffel-tower",
+        title: "Avengers",
+        url: "./assets/3.png",
+      },
+      {
+        id: "new-york-city",
+        title: "Short Stories",
+        url: "./assets/4.png",
+      },
+    ];
+    let prevoiusXPosition = -60;
+
+    for (var item of thumbNailsRef) {
+      const posX = prevoiusXPosition + 25;
+      const posY = 10;
+      const posZ = -40;
+      const position = { x: posX, y: posY, z: posZ };
+      prevoiusXPosition = posX;
+
+      // Border Element
+      const borderEl = this.createBorder(position, item.id);
+
+      // Thumbnail Element
+      const thumbNail = this.createThumbNail(item);
+      borderEl.appendChild(thumbNail);
+
+      // Title Text Element
+      const titleEl = this.createTitleEl(position, item);
+      borderEl.appendChild(titleEl);
+
+      this.placesContainer.appendChild(borderEl);
+    }
+  },
+  createBorder: function (position, id) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("id", id);
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      width:15,
+      height:21,
+    });
+    entityEl.setAttribute("position", position);
+    entityEl.setAttribute("material", {
+      color: "#0077CC",
+      opacity: 1,
+    });
+
+    //Add cursor-listener component to the ring border entity to change it's color 
+    //On Cursor 'mouseenter' and 'mouseleave' entity
+    entityEl.setAttribute("cl", {});
+
+    return entityEl;
+  },
+  createThumbNail: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("position", {x:0,y:0,z:0.1})
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      width:14,
+      height:20
+    });
+    entityEl.setAttribute("material", { src: item.url });
+
+    return entityEl;
+  },
+  createTitleEl: function (position, item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("text", {
+      font: "exo2bold",
+      align: "center",
+      width: 70,
+      color: "#e65100",
+      value: item.title,
+    });
+    const elPosition = position;
+    elPosition.y = -20;
+    entityEl.setAttribute("position", elPosition);
+    entityEl.setAttribute("visible", true);
+    
+    return entityEl;
+  },
+});
